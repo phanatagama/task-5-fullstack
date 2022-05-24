464870d28e8ee73b32bdbe4342a74a9dc439f5cf
phanatagama <phanatagama@gmail.com>
2022-05-24 23:38:23 +0700

    feat: change RegisterController & LoginController
    Signed-off-by: phanatagama <phanatagama@gmail.com>

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{    
+    /**
+     * login
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed!',
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Success!',
+            'data'    => $user,
+            'token'   => $user->createToken('authToken')->accessToken    
+        ]);
+    }
+    
+    /**
+     * logout
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $removeToken = $request->user()->tokens()->delete();
+
+        if($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Success!',  
+            ]);
+        }
+    }
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{    
+    /**
+     * register
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:8|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Register Success!',
+            'data'    => $user  
+        ]);
+    }
+}
