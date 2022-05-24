ab80fbecff1d15929fc0e188bc74899b024e2192
phanatagama <phanatagama@gmail.com>
2022-05-24 21:38:55 +0700

    feat: create RegisterController, LoginController & API Route
    Signed-off-by: phanatagama <phanatagama@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:api');
